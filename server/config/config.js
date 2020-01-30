a17cf6c865e0c5810900d7e445d93cf9fb854856
Prateek <prateek@Prateek.local>
2020-01-30 18:56:32 +0530

    adding cors

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 const env = process.env.NODE_ENV || "development";
 if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost/invoice';
+  process.env.PORT = 4000;
+  // process.env.MONGODB_URI = 'mongodb://localhost/invoice';
 } else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost/invoiceTest';
+  process.env.PORT = 4000;
+  // process.env.MONGODB_URI = 'mongodb://localhost/invoiceTest';
 }
--- server/server.js
+++ server/server.js
@@ -3,24 +3,31 @@ require("./config/config")
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+var cors = require('cors')
 const _ = require('lodash');
+// const {mongoose} = require('./db/mongoose');
+// const {Todo} = require('./models/todo');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-
+var corsOptions = {
+  origin: 'http://localhost:9000',
+  credentials: true
+}
 const app = express();
-
 app.use(bodyParser.json());
-
-app.post('/todos', (req, res) => {
-  const todo = new Todo({'text': req.body.text})
-  todo.save()
-    .then((doc) => {
-      res.send(doc)
-    })
-    .catch( (err) => {
-      res.status(400).send(err)
-    })
+app.use(cors(corsOptions))
+// app.options('*', cors());
+app.get('/todos',(req, res) => {
+  // const todo = new Todo({'text': req.body.text})
+  // todo.save()
+  //   .then((doc) => {
+  //     res.send(doc)
+  //   })
+  //   .catch( (err) => {
+  //     res.status(400).send(err)
+  //   })
+  setTimeout(() => {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:9000').status(502).send({type: "testing", message: "testing"})
+  }, 1000000)
 });
 
 
