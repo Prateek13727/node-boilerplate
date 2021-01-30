fd888dd7cbcc6b73bf7c3f91bda941b2a3810aeb
maverick <prateekpandey@theporter.in>
2021-01-31 03:27:58 +0530

    updating packages

--- server/server.js
+++ server/server.js
@@ -2,33 +2,50 @@ require("./config/config")
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
 var cors = require('cors')
 const _ = require('lodash');
+
+// const {ObjectID} = require('mongodb');
 // const {mongoose} = require('./db/mongoose');
 // const {Todo} = require('./models/todo');
 
 var corsOptions = {
-  origin: 'http://localhost:9000',
+  origin: 'http://localhost:8080',
   credentials: true
 }
 const app = express();
 app.use(bodyParser.json());
 app.use(cors(corsOptions))
-// app.options('*', cors());
-app.get('/todos',(req, res) => {
-  // const todo = new Todo({'text': req.body.text})
-  // todo.save()
-  //   .then((doc) => {
-  //     res.send(doc)
-  //   })
-  //   .catch( (err) => {
-  //     res.status(400).send(err)
-  //   })
+
+// app.post('/todo', (req,res) => {
+//   const todo = new Todo({'text': req.body.text})
+//   todo.save()
+//     .then((doc) => {
+//       res.send(doc)
+//     })
+//     .catch( (err) => {
+//       res.status(400).send(err)
+//     })
+// });
+
+app.get('/sucess', (req, res) => {
+  res.header("Access-Control-Allow-Origin", 'http://localhost:8080').status(200).send({type: "success", message: "Ready for the next"})
+})
+
+app.get('/timeout',(req, res) => {
   setTimeout(() => {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:9000').status(502).send({type: "testing", message: "testing"})
+    res.header("Access-Control-Allow-Origin", 'http://localhost:8080').status(502).send({type: "testing", message: "testing_timeout"})
   }, 1000000)
 });
+
+app.get('/error', (req, res) => {
+  res.header("Access-Control-Allow-Origin", 'http://localhost:8080').status(400).send({type: "Bad Request", message: "error"})
+})
+
+app.get('/unhandled', (req, res) => {
+  throw new Error()
+  res.header("Access-Control-Allow-Origin", 'http://localhost:8080').status(400).send({type: "Bad Request", message: "error"})
+})
 
 
 app.listen(process.env.PORT, () => {
